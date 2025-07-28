d61e1decb9db71309fa0c20af01b4c70df25a5ac
chaitanyawargand <chaitanyawargand@gmail.com>
2025-07-28 19:07:53 +0530

    Update usercontroller.js

--- backened/controllers/usercontroller.js
+++ backened/controllers/usercontroller.js
@@ -2,26 +2,56 @@ const User = require('./../modals/userschema');
 const catchAsync = require('./../utils/catchAsynch');
 const AppError = require('./../utils/apperror');
 const factory = require('./handlerFactory');
+const multer = require('multer');
+const sharp= require('sharp');
+const multerStorage = multer.memoryStorage();
 
-const filterObj =(obj, ...allowedFields)=>{
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an image! Please upload only images.', 400), false); // Reject the file
+  }
+};
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter
+})
+exports.uploadUserPhoto = upload.single('photo');
+
+exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
+  if (!req.file) return next();
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+  await sharp(req.file.buffer)
+    .resize(500, 500)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/images/${req.file.filename}`);
+  next();
+});
+const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
   Object.keys(obj).forEach(el => {
     if (allowedFields.includes(el)) newObj[el] = obj[el];
   });
   return newObj;
 };
-
-exports.getMe = (req,res,next) => {
+exports.getMe = (req, res, next) => {
   req.params.id = req.user.id;
   next();
 };
-
-exports.updateMe =catchAsync(async (req, res, next) => {
+exports.updateMe = catchAsync(async (req, res, next) => {
   if (req.body.password || req.body.passwordConfirm) {
-    return next(new AppError('This route is not for password updates. Please use /updateMyPassword.',400));
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updateMyPassword.',
+        400
+      )
+    );
   }
   const filteredBody = filterObj(req.body, 'name', 'email');
-  const updatedUser = await User.findByIdAndUpdate(req.user.id,filteredBody,{
+  if (req.file) filteredBody.photo = req.file.filename;
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
     new: true,
     runValidators: true
   });
@@ -32,7 +62,6 @@ exports.updateMe =catchAsync(async (req, res, next) => {
     }
   });
 });
-
 exports.deleteMe = catchAsync(async (req, res, next) => {
   await User.findByIdAndUpdate(req.user.id,{active: false });
   res.status(204).json({
@@ -44,6 +73,3 @@ exports.getUser = factory.getOne(User,{select: '-__v -password -passwordConfirm 
 exports.getAllUsers = factory.getAll(User,{select: '-__v -password -passwordConfirm -active'});
 exports.updateUser = factory.updateOne(User);
 exports.deleteUser = factory.deleteOne(User);
-
-
-

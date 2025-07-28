2c36b3a8d34cca7da2aa3ff78b2282f9b0110bc4
chaitanyawargand <chaitanyawargand@gmail.com>
2025-07-28 19:09:13 +0530

    Update app.js

--- backened/app.js
+++ backened/app.js
@@ -9,6 +9,7 @@ const globalErrorHandler=require('./controllers/errorcontroller')
 const eventroutes=require('./routes/eventroutes')
 const bookingroutes=require('./routes/bookingroutes')
 const app = express();
+const path = require('path');
 
 // Set security HTTP headers
 app.use(helmet());
@@ -18,22 +19,29 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 // app.use('/api', limiter);
-
 app.use(express.json({ limit: '10kb' }));
-
-// Data sanitization
 app.use(mongoSanitize());
 app.use(xss());
+const cors = require('cors');
+app.use(cors({
+  origin: ['http://localhost:3000', 'http://localhost:3001'],
+  credentials: true
+}));
+// Serve static files with CORS headers
+app.use('/images', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', req.headers.origin || 'http://localhost:3000');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+}, express.static(path.join(__dirname, 'public/images')));
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
 
-// routes
-app.use('/api/v1/users', userroutes);
-app.use('/api/v1/events', eventroutes);
-app.use('/api/v1/bookings', bookingroutes);
-
+app.use('/users', userroutes);
+app.use('/events', eventroutes);
+app.use('/bookings', bookingroutes);
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
-
 app.use(globalErrorHandler);
 
 module.exports = app;
